57297e9a6c01f45f89ff94071fdddf64abfa8a51
YTheLynch <yuganshu.yk@gmail.com>
2024-02-04 00:31:21 +0530

    Fixed price formatting and radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,8 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js'
+// .. indicates parent folder
+// . indicates current folder
 
 let productsHTML = '';
 
@@ -25,7 +28,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
