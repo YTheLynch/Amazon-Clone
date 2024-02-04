fd1b37b12956898f6751ca6ad3225ee26d25f187
YTheLynch <yuganshu.yk@gmail.com>
2024-02-04 12:09:51 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,30 +1,28 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export function addToCart(productId) {
     let matchingItem;
         
-        cart.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-                matchingItem = cartItem;
-                return;
-            }
-        });
-
-        if (matchingItem) {
-            matchingItem.quantity++;
-        } else {
-            cart.push({
-                productId, 
-                quantity: 1
-            });
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+            return;
         }
+    });
 
+    if (matchingItem) {
+        matchingItem.quantity++;
+    } else {
+        cart.push({
+            productId, 
+            quantity: 1
+        });
+    }
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function removeFromCart(productId) {
@@ -36,4 +34,5 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+    saveToStorage();
 }
